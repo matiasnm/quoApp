f71e913f47f3a88e5fe45c04a0fbb66b0ef8f59d
Matias Nehuen Malpartida <matiasnm@gmail.com>
2025-02-27 13:16:31 -0300

    always force https/disable cors

--- src/main/java/com/nocountry/quo/security/SecurityConfiguration.java
+++ src/main/java/com/nocountry/quo/security/SecurityConfiguration.java
@@ -24,17 +24,22 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.csrf(csrf -> csrf.disable())
-                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests((authorizeHttpRequests) ->
-                        authorizeHttpRequests.requestMatchers(HttpMethod.POST, "/login").permitAll()
-                        .requestMatchers("/coins", "/rates").permitAll()
-                        .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll()
-                        .anyRequest()
-                        .authenticated()
-                )
-                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
-                .build();
+        return httpSecurity
+            .cors(cors -> cors.disable())
+            .csrf(csrf -> csrf.disable())
+            .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .requiresChannel(channel -> channel
+                .anyRequest().requiresSecure() // Fuerza HTTPS en todas las solicitudes
+            )
+            .authorizeHttpRequests((authorizeHttpRequests) ->
+                authorizeHttpRequests.requestMatchers(HttpMethod.POST, "/login").permitAll()
+                .requestMatchers("/coins", "/rates").permitAll()
+                .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll()
+                .anyRequest()
+                .authenticated()
+            )
+            .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+            .build();
     }
 
     @Bean
