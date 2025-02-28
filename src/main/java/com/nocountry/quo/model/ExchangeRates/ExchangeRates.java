d8f4d17071011a55e949f82bf981df183bc9cdb6
Matias Nehuen Malpartida <matiasnm@gmail.com>
2025-02-27 22:57:45 -0300

    add ExchangeRates

---
+++ src/main/java/com/nocountry/quo/model/ExchangeRates/ExchangeRates.java
@@ -0,0 +1,36 @@
+package com.nocountry.quo.model.ExchangeRates;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import com.nocountry.quo.model.Enums.LocalCurrency;
+
+@Entity
+@Table(name = "exchange_rates")
+@Getter
+@AllArgsConstructor
+public class ExchangeRates {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Se guarda una tasa de cambio por día!
+    @Column(nullable = false, unique = true)
+    private LocalDate date;
+
+    @ElementCollection
+    @MapKeyEnumerated(EnumType.STRING)
+    @Column(name = "rate")
+    @CollectionTable(name = "exchange_rate_details", joinColumns = @JoinColumn(name = "exchange_rate_id"))
+    private Map<LocalCurrency, Double> rates = new HashMap<>();
+
+    public ExchangeRates(LocalDate date, Map<LocalCurrency, Double> rates) {
+        this.date = date;
+        this.rates = rates;
+    }
+}
