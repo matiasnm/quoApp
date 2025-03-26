497b35d38b6146be189bef0a2ab694d479782143
matiasnm <matiasnm@gmail.com>
2025-03-26 16:50:42 -0300

    add CORS

--- src/main/java/com/nocountry/quo/security/SecurityConfiguration.java
+++ src/main/java/com/nocountry/quo/security/SecurityConfiguration.java
@@ -25,7 +25,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-            .cors(cors -> cors.disable())
+            .cors(cors -> {})
             .csrf(csrf -> csrf.disable())
             .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests((authorizeHttpRequests) ->
