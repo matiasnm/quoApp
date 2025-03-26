7906b740e455b9bda04c13b4fb4b8ed0ee501a53
matiasnm <matiasnm@gmail.com>
2025-03-26 19:51:03 -0300

    cors *

--- src/main/java/com/nocountry/quo/config/GlobalConfig.java
+++ src/main/java/com/nocountry/quo/config/GlobalConfig.java
@@ -14,7 +14,7 @@ public class GlobalConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5500") // or "*" for all
+                        .allowedOrigins("*") // or "*" for all; http://127.0.0.1:5500
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
